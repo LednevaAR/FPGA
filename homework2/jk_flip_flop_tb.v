48fbc30a182bc82c7a43875e93a6383bd1066386
LednevaAR <81328783+LednevaAR@users.noreply.github.com>
2023-11-07 17:52:19 +0300

    Add files via upload

---
+++ homework2/jk_flip_flop_tb.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ns 
+module tb;
+	reg j, k, rstn, clk;
+	wire q;
+	integer i;
+	reg [2:0] dly;
+	always #10 clk = ~clk;
+	jk_flip_flop u0 (.j(j), .k(k), .clk(clk), .rstn(rstn), .q(q));
+	initial begin
+		{j, k, rstn, clk} <= 0;
+		#10 rstn <= 1;
+		for (i = 0; i < 10; i = i+1) begin
+			dly = $random;
+			#(dly) j <= $random;
+			#(dly) k <= $random;
+		end
+		#20 $finish;
+	end
+endmodule
